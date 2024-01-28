53f5510fc8eb899ec6ebdaf9194b8c9496619b49
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2024-01-28 19:32:46 +0700

    Create: sign in template

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
 import '@/style.css'
 
 import { createApp } from 'vue'
+// Library dependencies
 import { createPinia } from 'pinia'
+import { Icon } from '@iconify/vue'
+import router from './router'
+import { plugin, defaultConfig } from '@formkit/vue'
+// end library dependencies
 
 import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
-
+app.component('IconVue', Icon)
 app.use(createPinia())
+app.use(plugin, defaultConfig)
 app.use(router)
 
 app.mount('#app')
