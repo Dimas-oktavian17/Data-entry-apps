c2bfe530ddb8d04101361ce8d7e80d49a8704e14
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2024-02-01 14:23:58 +0700

    feat: config vuefire

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import '@/style.css'
 
 import { createApp } from 'vue'
 // Library dependencies
+import { VueFire, VueFireAuth } from 'vuefire'
 import { createPinia } from 'pinia'
 import { Icon } from '@iconify/vue'
 import router from './router'
 import { plugin, defaultConfig } from '@formkit/vue'
 // end library dependencies
-
+// firebase configuration
+import { firebaseApp } from './firebase/index.js'
 import App from './App.vue'
 
 const app = createApp(App)
@@ -15,5 +17,12 @@ app.component('IconVue', Icon)
 app.use(createPinia())
 app.use(plugin, defaultConfig)
 app.use(router)
-
-app.mount('#app')
+app.use(VueFire, {
+  // imported above but could also just be created here
+  firebaseApp,
+  modules: [
+    // we will see other modules later on
+    VueFireAuth(),
+  ],
+})
+  .mount('#app')
