a93be7c428e1abcdc8fb1b07fc5485a72f47b64c
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2024-02-05 21:17:20 +0700

    feat: add ResetPassword.vue, create all functionally sign-up, sign-in & auh google auth

--- src/main.js
+++ src/main.js
@@ -9,29 +9,25 @@ import router from './router'
 import { plugin, defaultConfig } from '@formkit/vue'
 // end library dependencies
 // firebase configuration
-// import '@/config/trusted-security-policies'
 import { firebaseApp } from './firebase/index.js'
+// Component configuration
+import NotifError from "@/components/base/NotifError.vue"
+import buttonAuth from "@/components/base/ButtonAuth.vue"
 import App from './App.vue'
-// import '@/trusted-security-policies'
-import { trustedTypes } from 'trusted-types'
-if (window.trustedTypes && trustedTypes.createPolicy) { // Feature detection
-  const escapeHTMLPolicy = trustedTypes.createPolicy('myEscapePolicy', {
-    createHTML: (string) => string.replace(/>/g, ">"),
-  });
-}
-if (typeof trustedTypes == 'undefined') { trustedTypes = { createPolicy: (n, rules) => rules } }
 
 const app = createApp(App)
 app.component('IconVue', Icon)
-app.use(createPinia())
-app.use(plugin, defaultConfig)
-app.use(router)
-app.use(VueFire, {
-  // imported above but could also just be created here
-  firebaseApp,
-  modules: [
-    // we will see other modules later on
-    VueFireAuth(),
-  ],
-})
+  .component('NotifError', NotifError)
+  .component('ButtonAuth', buttonAuth)
+  .use(createPinia())
+  .use(plugin, defaultConfig)
+  .use(router)
+  .use(VueFire, {
+    // imported above but could also just be created here
+    firebaseApp,
+    modules: [
+      // we will see other modules later on
+      VueFireAuth(),
+    ],
+  })
   .mount('#app')
