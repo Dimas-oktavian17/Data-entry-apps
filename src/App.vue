0fb40803fe7abd43cdddd3bea6fd764c42ec05c1
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2024-02-13 22:46:13 +0700

    feat: setup api and create some function location api

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,9 @@
 <script setup>
 import { onMounted } from 'vue';
 import { RouterView, } from 'vue-router'
-import locationAPI from '@/services/locationAPI'
 
-onMounted(async () => {
- try {
-  const { data } = await locationAPI.getLocation()
-  console.table(data);
- } catch (error) {
-  console.error(error);
- }
-})
 </script>
 
 <template>
- <RouterView />
+    <RouterView />
 </template>
