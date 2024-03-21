4d441216da09b45db48b1a90b6fa59b343c88a75
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2024-03-21 09:15:31 +0700

    WIP: creating data chart by date month

--- src/views/Dashboard/ECommerceView.vue
+++ src/views/Dashboard/ECommerceView.vue
@@ -9,7 +9,7 @@
 
   <div class="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
    <!-- ====== Chart One Start -->
-   <!-- <ChartOne /> -->
+   <ChartOne />
    <!-- ====== Chart One End -->
 
    <!-- ====== Chart Two Start -->
