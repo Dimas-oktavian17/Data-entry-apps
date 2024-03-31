98eb68dcc737d1ee8d01d9150b1c5618fcc5c0fa
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2024-03-31 12:34:30 +0700

    feat: Creating dynamic routing edits form

--- src/components/Tables/TableThree.vue
+++ src/components/Tables/TableThree.vue
@@ -1,64 +1,59 @@
 <script setup>
-import BreadcrumbDefault from '@/components/Breadcrumbs/BreadcrumbDefault.vue'
-import DefaultCard from '@/components/Forms/DefaultCard.vue'
-import InputGroup from '@/components/Forms/InputGroup.vue'
-import SelectGroup from '@/components/Forms/SelectGroup.vue'
-import { excelStore } from '@/stores/excel/excelStore';
-import { onMounted, ref, watchEffect, watch } from 'vue'
+import {
+ onMounted,
+ // ref,
+ watchEffect, watch
+} from 'vue'
 import { storeToRefs } from 'pinia'
-import { UsersPinia } from '@/stores/users/users'
+// import { UsersPinia } from '@/stores/users/users'
 import { formPinia } from '@/stores/formAPI/index'
 import { formUsers } from '@/stores/users/formUsers';
+import { excelStore } from '@/stores/users/updateUsers';
 
 const formStore = formPinia()
-const Users = UsersPinia()
+// const Users = UsersPinia()
 const FormUsers = formUsers()
 // Author information
-const { name, Email, photo, uid } = storeToRefs(Users)
+// const { name, Email, photo, uid } = storeToRefs(Users)
 const {
- provinces,
+ // provinces,
  cities,
  kecamatan,
  kelurahan,
  filterUsers,
 } = storeToRefs(formStore)
 const {
- formID,
- names,
- age,
- position,
- statusKaryawan,
+ // formID,
+ // names,
+ // age,
+ // position,
+ // statusKaryawan,
  selectedProvince,
  selectedCity,
  selectedDistrict,
- selectedVillages,
- AlertForm,
- open,
- dataView
+ selectedVillages
 } = storeToRefs(FormUsers)
-
-const handleView = (name, id) => FormUsers.HandleView(name, id)
 // data form
 const handleDelete = (index) => FormUsers.HandleDelete(index)
-const handleEdit = async (id) => FormUsers.HandleEdit(id)
-const handleSubmit = async () => FormUsers.HandleUpdate(
- formID.value,
- name.value,
- Email.value,
- uid.value,
- photo.value,
- names.value,
- age.value,
- position.value,
- statusKaryawan.value,
- selectedProvince.value,
- selectedCity.value,
- selectedDistrict.value,
- selectedVillages.value,
-)
-const isOptionSelected = ref(false)
-const changeTextColor = () => isOptionSelected.value = true
-const pageTitle = ref('Form Layout')
+// const handleEdit = async (id) => FormUsers.HandleEdit(id)
+// const handleSubmit = async () => FormUsers.HandleUpdate(
+//  formID.value,
+//  name.value,
+//  Email.value,
+//  uid.value,
+//  photo.value,
+//  names.value,
+//  age.value,
+//  position.value,
+//  statusKaryawan.value,
+//  selectedProvince.value,
+//  selectedCity.value,
+//  selectedDistrict.value,
+//  selectedVillages.value,
+// )
+// const isOptionSelected = ref(false)
+// const changeTextColor = () => isOptionSelected.value = true
+// const pageTitle = ref('Form Layout')
 onMounted(async () => formStore.LoadProvinces())
 // Fetching data 
 const fetchProvinces = async ({ id }) => formStore.fetchProvinces({ id }, selectedCity.value, selectedDistrict.value)
@@ -90,7 +85,7 @@ watchEffect(() => selectedDistrict.value !== null && handleDistrict())
 </script>
 
 <template>
- <div v-if="AlertForm === false"
+ <div
   class="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
   <header class="grid grid-cols-1 place-items-end">
    <!-- {{ testUser }} -->
@@ -137,10 +132,10 @@ watchEffect(() => selectedDistrict.value !== null && handleDistrict())
       </td>
       <td class="px-4 py-5">
        <p class="inline-flex px-3 py-1 text-sm font-medium rounded-full bg-opacity-10" :class="{
-  'bg-warning text-warning': status_karyawan === 'kontrak',
-  'bg-danger text-danger': status_karyawan === 'magang',
-  'bg-success text-success': status_karyawan === 'kartap'
- }">
+     'bg-warning text-warning': status_karyawan === 'kontrak',
+     'bg-danger text-danger': status_karyawan === 'magang',
+     'bg-success text-success': status_karyawan === 'kartap'
+    }">
         {{ status_karyawan }}
        </p>
       </td>
@@ -149,144 +144,19 @@ watchEffect(() => selectedDistrict.value !== null && handleDistrict())
         <router-link :to="{ name: 'tablesDetails', params: { id: id } }">
          <IconVue icon="ph:eye-bold" class="hover:text-primary w-[18px] h-auto" />
         </router-link>
-        <!-- <button @click="handleView(name, id)">
-         <IconVue icon="ph:eye-bold" class="hover:text-primary w-[18px] h-auto" />
-        </button> -->
-
+        <!--  -->
         <button @click="handleDelete(id)" class="hover:text-primary">
          <IconVue icon="ion:trash-outline" class="hover:text-primary w-[18px] h-auto" />
         </button>
-
-        <button @click="handleEdit(id)" class="hover:text-primary">
+        <!-- @click="handleEdit(id)" -->
+        <router-link :to="{ name: 'tablesEdits', params: { id: id } }" class="hover:text-primary">
          <IconVue icon="akar-icons:edit" class="hover:text-primary w-[18px] h-auto" />
-        </button>
+        </router-link>
        </div>
       </td>
      </tr>
     </tbody>
    </table>
   </div>
- </div>
- <!-- edit form -->
- <div class="" v-if="AlertForm !== false">
-  <!-- Breadcrumb Start -->
-  <BreadcrumbDefault :pageTitle="pageTitle" />
-  <!-- Breadcrumb End -->
-  <!-- ====== Form Layout Section Start -->
-  <div class="grid grid-cols-1 gap-9 ">
-   <div class="flex flex-col gap-9">
-    <!-- Contact Form Start -->
-    <DefaultCard cardTitle="Contact Form">
-     <form @submit.prevent="handleSubmit">
-      <div class="p-6.5">
-       <div class="mb-4.5 flex flex-col gap-6 xl:flex-row">
-        <InputGroup v-model="names" label="Your name" type="text" placeholder="Your name"
-         customClasses="w-full xl:w-1/2" />
-        <InputGroup v-model="age" label="Your age" type="number" placeholder="Your age"
-         customClasses="w-full xl:w-1/2" />
-       </div>
-       <InputGroup v-model="position" label="Position" type="text" placeholder="Your position" customClasses="mb-4.5"
-        required />
-       <SelectGroup v-model="statusKaryawan" />
-       <!-- location api -->
-       <div class="mb-4.5">
-        <label class="mb-2.5 block text-black dark:text-white mt-2.5">Province</label>
-        <div class="relative z-20 bg-transparent dark:bg-form-input">
-         <select v-model="selectedProvince"
-          class="relative z-20 w-full px-5 py-3 transition bg-transparent border rounded outline-none appearance-none border-stroke focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-          :class="{ 'text-black dark:text-white': isOptionSelected }" @change="changeTextColor">
-          <option value="" disabled selected>Type your subject</option>
-          <option v-for="( item, index ) in provinces" :key="index" :value="item">
-           {{ item.name }}
-          </option>
-         </select>
-         <span class="absolute z-30 -translate-y-1/2 top-1/2 right-4">
-          <svg class="fill-current" width="24" height="24" viewBox="0 0 24 24" fill="none"
-           xmlns="http://www.w3.org/2000/svg">
-           <g opacity="0.8">
-            <path fill-rule="evenodd" clip-rule="evenodd"
-             d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
-             fill=""></path>
-           </g>
-          </svg>
-         </span>
-        </div>
-        <!-- City -->
-        <label class="mb-2.5 block text-black dark:text-white mt-2.5">City</label>
-        <div class="relative z-20 bg-transparent dark:bg-form-input">
-         <select :disabled="cities === null" v-model="selectedCity"
-          class="relative z-20 w-full px-5 py-3 transition bg-transparent border rounded outline-none appearance-none border-stroke focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-          :class="{ 'text-black dark:text-white': isOptionSelected }" @change="changeTextColor">
-          <option value="" disabled selected>Type your subject</option>
-          <option v-for="( item, index ) in cities" :key="index" :value="item">{{ item.name }}</option>
-         </select>
-         <span class="absolute z-30 -translate-y-1/2 top-1/2 right-4">
-          <svg class="fill-current" width="24" height="24" viewBox="0 0 24 24" fill="none"
-           xmlns="http://www.w3.org/2000/svg">
-           <g opacity="0.8">
-            <path fill-rule="evenodd" clip-rule="evenodd"
-             d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
-             fill=""></path>
-           </g>
-          </svg>
-         </span>
-        </div>
-        <!-- end city -->
-        <!-- kecamatan -->
-        <label class="mb-2.5 block text-black dark:text-white mt-2.5">Kecamatan</label>
-        <div class="relative z-20 bg-transparent dark:bg-form-input">
-         <select :disabled="selectedCity === null" v-model="selectedDistrict"
-          class="relative z-20 w-full px-5 py-3 transition bg-transparent border rounded outline-none appearance-none border-stroke focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-          :class="{ 'text-black dark:text-white': isOptionSelected }" @change="changeTextColor">
-          <option value="" disabled selected>Type your subject</option>
-          <option v-for="(item, index) in kecamatan" :key="index" :value="item">{{ item.name }}</option>
-         </select>
-         <span class="absolute z-30 -translate-y-1/2 top-1/2 right-4">
-          <svg class="fill-current" width="24" height="24" viewBox="0 0 24 24" fill="none"
-           xmlns="http://www.w3.org/2000/svg">
-           <g opacity="0.8">
-            <path fill-rule="evenodd" clip-rule="evenodd"
-             d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
-             fill=""></path>
-           </g>
-          </svg>
-         </span>
-        </div>
-        <!-- end kecamatan -->
-        <!-- kelurahan -->
-        <label class="mb-2.5 block text-black dark:text-white mt-2.5">Kelurahan</label>
-        <div class="relative z-20 bg-transparent dark:bg-form-input">
-         <select :disabled="selectedDistrict === null" v-model="selectedVillages"
-          class="relative z-20 w-full px-5 py-3 transition bg-transparent border rounded outline-none appearance-none border-stroke focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-          :class="{ 'text-black dark:text-white': isOptionSelected }" @change="changeTextColor">
-          <option value="" disabled selected>Type your subject</option>
-          <option v-for="(item, index) in kelurahan" :key="index" :value="item">{{ item.name }}</option>
-         </select>
-         <span class="absolute z-30 -translate-y-1/2 top-1/2 right-4">
-          <svg class="fill-current" width="24" height="24" viewBox="0 0 24 24" fill="none"
-           xmlns="http://www.w3.org/2000/svg">
-           <g opacity="0.8">
-            <path fill-rule="evenodd" clip-rule="evenodd"
-             d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
-             fill=""></path>
-           </g>
-          </svg>
-         </span>
-        </div>
-        <!-- end kelurahan -->
-       </div>
-       <!-- end location api -->
-       <button class="flex justify-center w-full p-3 font-medium rounded bg-primary text-gray hover:bg-opacity-90">
-        Send Message
-       </button>
-      </div>
-     </form>
-    </DefaultCard>
-    <!-- Contact Form End -->
-   </div>
-
-
-  </div>
-  <!-- ====== Form Layout Section End -->
  </div>
 </template>
---
+++ src/views/Tables/TablesEdits.vue
@@ -0,0 +1,223 @@
+<script setup>
+import { onMounted, watchEffect, watch, ref } from 'vue';
+import { formPinia } from '@/stores/formAPI/index'
+import { UsersPinia } from '@/stores/users/users'
+import { formUsers } from '@/stores/users/formUsers';
+import { TableStore } from "@/stores/tables/tableStore"
+import { storeToRefs } from 'pinia';
+import { useRoute } from 'vue-router';
+const route = useRoute()
+const ID = ref(route.params.id)
+const formStore = formPinia()
+const FormUsers = formUsers()
+const Users = UsersPinia()
+const {
+ provinces,
+ cities,
+ kecamatan,
+ kelurahan,
+} = storeToRefs(formStore)
+const {
+ AlertsStatus,
+ names,
+ age,
+ position,
+ statusKaryawan,
+ selectedProvince,
+ selectedCity,
+ selectedDistrict,
+ selectedVillages
+} = storeToRefs(FormUsers)
+// Author information
+const isOptionSelected = ref(false)
+// const AlertsStatus = ref(false)
+const changeTextColor = () => isOptionSelected.value = true
+const { name, Email, photo, uid } = storeToRefs(Users)
+const pageTitle = ref('Form Layout')
+const handleSubmit = async () => FormUsers.HandleUpdate(
+ ID.value,
+ name.value,
+ Email.value,
+ uid.value,
+ photo.value,
+ names.value,
+ age.value,
+ position.value,
+ statusKaryawan.value,
+ selectedProvince.value,
+ selectedCity.value,
+ selectedDistrict.value,
+ selectedVillages.value,
+)
+// Fetching data 
+const fetchProvinces = async ({ id }) => formStore.fetchProvinces({ id }, selectedCity.value, selectedDistrict.value)
+const fecthCity = async ({ id }) => formStore.fecthCity({ id }, selectedDistrict.value)
+const fetchDistrict = async ({ id }) => formStore.fetchDistrict({ id }, selectedVillages.value)
+// Watch effect for data form
+watch(selectedProvince, fetchProvinces, { immediate: true })
+watch(selectedCity, fecthCity, { immediate: true })
+watch(selectedDistrict, fetchDistrict, { immediate: true })
+const handleProvince = () => FormUsers.HandleProvince(
+ selectedCity.value,
+ selectedDistrict.value,
+ selectedVillages.value,
+ cities.value,
+ kecamatan.value,
+ kelurahan.value
+)
+const handleCity = () => FormUsers.HandleCity(
+ selectedDistrict.value,
+ kecamatan.value,
+ kelurahan.value,
+ selectedVillages.value
+)
+const handleDistrict = () => FormUsers.HandleDistrict(kelurahan.value, selectedVillages.value)
+
+watchEffect(() => selectedProvince.value !== null && handleProvince())
+watchEffect(() => selectedCity.value !== null && handleCity())
+watchEffect(() => selectedDistrict.value !== null && handleDistrict())
+onMounted(() => TableStore().TableView())
+</script>
+<template>
+ <DefaultLayout>
+  <!-- edit form -->
+  <!-- Breadcrumb Start -->
+  <BreadcrumbDefault :pageTitle="pageTitle" />
+  <!-- Breadcrumb End -->
+  <!-- ====== Form Layout Section Start -->
+  <div class="grid grid-cols-1 gap-9 ">
+   <div class="flex flex-col gap-9">
+    <!-- Contact Form Start -->
+    <DefaultCard cardTitle="Contact Form">
+     <AlertSuccess v-if="AlertsStatus" :title="names" />
+     <FormKit type="form" @submit="handleSubmit">
+      <div class="p-6.5">
+       <div class="mb-4.5 flex flex-col gap-6 xl:flex-row">
+        <FormKit v-model="names" type="text" name="name" label="Your name" placeholder="Abu Na'im"
+         validation="required|length:5,15" :classes="{
+   outer: 'mb-4.5 w-full xl:w-1/2',
+   label:
+    'mb-2.5 block text-black dark:text-white',
+   inner: ' focus:outline-1',
+   input:
+    'w-full rounded border-[1.5px] text-black border-stroke bg-transparent py-3 px-5 font-normal outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:text-white dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary',
+  }" />
+        <FormKit v-model="age" type="number" name="age" label="Your age" placeholder="Your age" validation="required"
+         :classes="{
+   outer: 'mb-4.5 w-full xl:w-1/2',
+   label:
+    'mb-2.5 block text-black dark:text-white',
+   inner: ' focus:outline-1',
+   input:
+    'w-full rounded border-[1.5px] text-black border-stroke bg-transparent py-3 px-5 font-normal outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:text-white dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary',
+  }" />
+       </div>
+       <FormKit v-model="position" type="text" name="Position" label="Your position" placeholder="IT Staff"
+        validation="required|length:5,15" :classes="{
+   outer: 'mb-4.5 w-full ',
+   label:
+    'mb-2.5 block text-black dark:text-white',
+   inner: ' focus:outline-1',
+   input:
+    'w-full rounded border-[1.5px] text-black border-stroke bg-transparent py-3 px-5 font-normal outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:text-white dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary',
+  }" />
+       <SelectGroup v-model="statusKaryawan" />
+       <!-- location api -->
+       <div class="mb-4.5">
+        <label class="mb-2.5 block text-black dark:text-white mt-2.5">Province</label>
+        <div class="relative z-20 bg-transparent dark:bg-form-input">
+         <select v-model="selectedProvince"
+          class="relative z-20 w-full px-5 py-3 transition bg-transparent border rounded outline-none appearance-none border-stroke focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+          :class="{ 'text-black dark:text-white': isOptionSelected }" @change="changeTextColor">
+          <option value="" disabled selected>Type your subject</option>
+          <option v-for="( item, index ) in provinces" :key="index" :value="item">
+           {{ item.name }}
+          </option>
+         </select>
+         <span class="absolute z-30 -translate-y-1/2 top-1/2 right-4">
+          <svg class="fill-current" width="24" height="24" viewBox="0 0 24 24" fill="none"
+           xmlns="http://www.w3.org/2000/svg">
+           <g opacity="0.8">
+            <path fill-rule="evenodd" clip-rule="evenodd"
+             d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
+             fill=""></path>
+           </g>
+          </svg>
+         </span>
+        </div>
+        <!-- City -->
+        <label class="mb-2.5 block text-black dark:text-white mt-2.5">City</label>
+        <div class="relative z-20 bg-transparent dark:bg-form-input">
+         <select :disabled="cities === null" v-model="selectedCity"
+          class="relative z-20 w-full px-5 py-3 transition bg-transparent border rounded outline-none appearance-none border-stroke focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+          :class="{ 'text-black dark:text-white': isOptionSelected }" @change="changeTextColor">
+          <option value="" disabled selected>Type your subject</option>
+          <option v-for="( item, index ) in cities" :key="index" :value="item">{{ item.name }}</option>
+         </select>
+         <span class="absolute z-30 -translate-y-1/2 top-1/2 right-4">
+          <svg class="fill-current" width="24" height="24" viewBox="0 0 24 24" fill="none"
+           xmlns="http://www.w3.org/2000/svg">
+           <g opacity="0.8">
+            <path fill-rule="evenodd" clip-rule="evenodd"
+             d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
+             fill=""></path>
+           </g>
+          </svg>
+         </span>
+        </div>
+        <!-- end city -->
+        <!-- kecamatan -->
+        <label class="mb-2.5 block text-black dark:text-white mt-2.5">Kecamatan</label>
+        <div class="relative z-20 bg-transparent dark:bg-form-input">
+         <select :disabled="selectedCity === null" v-model="selectedDistrict"
+          class="relative z-20 w-full px-5 py-3 transition bg-transparent border rounded outline-none appearance-none border-stroke focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+          :class="{ 'text-black dark:text-white': isOptionSelected }" @change="changeTextColor">
+          <option value="" disabled selected>Type your subject</option>
+          <option v-for="(item, index) in kecamatan" :key="index" :value="item">{{ item.name }}</option>
+         </select>
+         <span class="absolute z-30 -translate-y-1/2 top-1/2 right-4">
+          <svg class="fill-current" width="24" height="24" viewBox="0 0 24 24" fill="none"
+           xmlns="http://www.w3.org/2000/svg">
+           <g opacity="0.8">
+            <path fill-rule="evenodd" clip-rule="evenodd"
+             d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
+             fill=""></path>
+           </g>
+          </svg>
+         </span>
+        </div>
+        <!-- end kecamatan -->
+        <!-- kelurahan -->
+        <label class="mb-2.5 block text-black dark:text-white mt-2.5">Kelurahan</label>
+        <div class="relative z-20 bg-transparent dark:bg-form-input">
+         <select :disabled="selectedDistrict === null" v-model="selectedVillages"
+          class="relative z-20 w-full px-5 py-3 transition bg-transparent border rounded outline-none appearance-none border-stroke focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+          :class="{ 'text-black dark:text-white': isOptionSelected }" @change="changeTextColor">
+          <option value="" disabled selected>Type your subject</option>
+          <option v-for="(item, index) in kelurahan" :key="index" :value="item">{{ item.name }}</option>
+         </select>
+         <span class="absolute z-30 -translate-y-1/2 top-1/2 right-4">
+          <svg class="fill-current" width="24" height="24" viewBox="0 0 24 24" fill="none"
+           xmlns="http://www.w3.org/2000/svg">
+           <g opacity="0.8">
+            <path fill-rule="evenodd" clip-rule="evenodd"
+             d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
+             fill=""></path>
+           </g>
+          </svg>
+         </span>
+        </div>
+        <!-- end kelurahan -->
+       </div>
+       <!-- end location api -->
+       <button class="flex justify-center w-full p-3 font-medium rounded bg-primary text-gray hover:bg-opacity-90">
+        Send Message
+       </button>
+      </div>
+     </FormKit>
+    </DefaultCard>
+   </div>
+  </div>
+  <!-- ====== Form Layout Section End -->
+ </DefaultLayout>
+</template>
