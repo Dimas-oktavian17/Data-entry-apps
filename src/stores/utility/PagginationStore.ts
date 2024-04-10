6c99e63f455278a63192d389bdf2511694c84738
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2024-04-10 10:18:38 +0700

    feat: creating reusable buttonPaggination and store to pinia

---
+++ src/stores/utility/PagginationStore.ts
@@ -0,0 +1,42 @@
+import { computed, ref } from 'vue'
+import { defineStore } from 'pinia'
+import { formPinia } from '@/stores/formAPI/index';
+// Define a type or interface for the array of numbers
+type NumberCount = number;
+export const PagginationStore = defineStore('PagginationStore', () => {
+ // state
+ const DataUsers = formPinia()
+ const page = ref<NumberCount>(1)
+ const pageSize = ref<NumberCount>(5)
+ const data = ref([])
+ // getters
+ const RealData = computed(() => data.value)
+ // Actions
+ function fetch(page: NumberCount, pageSize: NumberCount) {
+  return new Promise((resolve) => {
+   const start = (page - 1) * pageSize
+   const end = start + pageSize
+   setTimeout(() => {
+    resolve(DataUsers.filterUsers.slice(start, end))
+   }, 100)
+  })
+ }
+ fetchData({
+  currentPage: page.value,
+  currentPageSize: pageSize.value,
+ })
+
+ function fetchData({ currentPage, currentPageSize }: { currentPage: number, currentPageSize: number }) {
+  fetch(currentPage, currentPageSize).then((responseData) => data.value = responseData)
+ }
+ return {
+  DataUsers,
+  page,
+  pageSize,
+  data,
+  RealData,
+  fetch,
+  fetchData
+ }
+})
+
